dbfddf1d493d245daa106ef8483761f3562c65c1
proghasan <mohammaddothasan@gmail.com>
2022-05-13 19:46:46 +0600

    learn slice

---
+++ 6.slice/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to slice")
+
+	var fruits = []string{"Apple", "Jack fruit", "Banana"}
+	fmt.Println("Fruits list", fruits)
+	fmt.Println()
+
+	// iteration with range
+	for index, value := range fruits {
+		fmt.Printf("index number %d and value is %s \n", index, value)
+	}
+}
